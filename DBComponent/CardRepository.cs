311c13e1fd43c93278bbc2e8e7acac0db00b2715
Chikin Pavel <chpe22u995@student.bmstu.ru>
2025-05-22 15:02:00 +0300

    Added DBComponent

---
+++ DBComponent/CardRepository.cs
@@ -0,0 +1,28 @@
+﻿using BLComponent;
+using BLComponent.InputPorts;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+
+namespace DBComponent;
+
+public class CardRepository(IConfiguration config) : ICardRepository
+{
+    private readonly Connection _connection = new(config);
+
+    public IList<Card> GetAll
+    {
+        get
+        {
+            var conn = new NpgsqlConnection(_connection.GetDefaultConnectionString);
+            var cards = new List<Card>();
+            conn.Open();
+            using var cmd = new NpgsqlCommand("SELECT * FROM decks.ClassicDeck", conn);
+            using var reader = cmd.ExecuteReader();
+            while (reader.Read())
+                cards.Add(CardFactory.CreateCard((CardName)reader["Name"],
+                    (CardSuit)reader["Suit"], (CardRank)reader["Rank"]));
+            conn.Close();
+            return cards;
+        }
+    }
+}
---
+++ DBComponent/Connection.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace DBComponent;
+
+public class Connection(IConfiguration config)
+{
+    public string? GetDefaultConnectionString => config["ConnectionStrings:DefaultConnection"];
+}
