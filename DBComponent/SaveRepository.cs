ee49ab573a2a56e1e20225fb9af5b49d1fee90fd
Chikin Pavel <chpe22u995@student.bmstu.ru>
2025-05-23 13:08:24 +0300

    Added DBUnitTests

--- DBComponent/SaveRepository.cs
+++ DBComponent/SaveRepository.cs
@@ -1,71 +1,72 @@
 ﻿using BLComponent;
 using BLComponent.InputPorts;
 using Microsoft.Data.Sqlite;
+using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 
 namespace DBComponent;
 
-public sealed class SaveRepository : ISaveRepository, IDisposable
+public class SaveRepository : ISaveRepository, IDisposable
 {
-    private readonly SqliteConnection _connection;
+    protected readonly SqliteConnection Connection;
 
-    public SaveRepository()
+    public SaveRepository(IConfiguration config)
     {
         var appData = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
-        var dbPath = Path.Combine(appData, "Bang!", "saves.db");
+        var dbPath = Path.Combine(appData, config["SavesFileName"]!);
         
         Directory.CreateDirectory(Path.GetDirectoryName(dbPath)!);
-        _connection = new SqliteConnection($"Data Source={dbPath}");
-        _connection.Open();
+        Connection = new SqliteConnection($"Data Source={dbPath}");
+        Connection.Open();
         
-        using var command = _connection.CreateCommand();
+        using var command = Connection.CreateCommand();
         command.CommandText = "CREATE TABLE IF NOT EXISTS states (" + 
                               "id INTEGER PRIMARY KEY AUTOINCREMENT, " + 
                               "created INTEGER NOT NULL, " + 
                               "state TEXT NOT NULL)";
         command.ExecuteNonQuery();
         
-        _connection.Close();
+        Connection.Close();
     }
 
     public Dictionary<int, long> GetAll
     {
         get
         {
-            _connection.Open();
-            using var command = _connection.CreateCommand();
+            Connection.Open();
+            using var command = Connection.CreateCommand();
             command.CommandText = "SELECT id, created FROM states ORDER BY created DESC";
             using var reader = command.ExecuteReader();
             var results = new Dictionary<int, long>();
             while (reader.Read())
                 results.Add(reader.GetInt32(0), reader.GetInt64(1));
-            _connection.Close();
+            Connection.Close();
             return results;
         }
     }
 
     public GameStateDto FindState(int stateId)
     {
-        _connection.Open();
-        using var command = _connection.CreateCommand();
+        Connection.Open();
+        using var command = Connection.CreateCommand();
         command.CommandText = $"SELECT state FROM states WHERE id = {stateId}";
         using var reader = command.ExecuteReader();
         reader.Read();
         var json = reader.GetString(0);
-        _connection.Close();
+        Connection.Close();
         return JsonConvert.DeserializeObject<GameStateDto>(json)!;
     }
 
     public void SaveState(GameStateDto state)
     {
-        _connection.Open();
-        using var command = _connection.CreateCommand();
+        Connection.Open();
+        using var command = Connection.CreateCommand();
         var json = JsonConvert.SerializeObject(state);
         command.CommandText = "INSERT INTO states (created, state) " +
             $"VALUES ({DateTimeOffset.UtcNow.ToUnixTimeSeconds()}, '{json}')";
         command.ExecuteNonQuery();
-        _connection.Close();
+        Connection.Close();
     }
 
-    public void Dispose() => _connection.Dispose();
+    public void Dispose() => Connection.Dispose();
 }
---
+++ DBUnitTests/SaveRepositoryForTests.cs
@@ -0,0 +1,16 @@
+﻿using DBComponent;
+using Microsoft.Extensions.Configuration;
+
+namespace DBUnitTests;
+
+public class SaveRepositoryForTests : SaveRepository
+{
+    public SaveRepositoryForTests(IConfiguration config) : base(config)
+    {
+        Connection.Open();
+        using var command = Connection.CreateCommand();
+        command.CommandText = "DELETE FROM states;";
+        command.ExecuteNonQuery();
+        Connection.Close();
+    }
+}
